5113325d89d2385cfa1e225df5ddedcdc1216cf9
zjx <1193028309@qq.com>
2022-11-24 00:28:12 +0800

    setRem背景设置完成

---
+++ js/setRem.js
@@ -0,0 +1,10 @@
+function setRem() {
+  var cli_W = document.documentElement.clientWidth || document.body.clientWidth;
+  cli_W = cli_W > 1920 ? 1920 : cli_W;
+  cli_W = cli_W < 1024 ? 1024 : cli_W;
+  var html = document.getElementsByTagName("html")[0];
+  var rem = cli_W / 24;
+  html.style.fontsize = rem + "px";
+}
+window.onload = setRem;
+window.onresize = setRem;
